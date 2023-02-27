27208e83dd2293f9d1f2b649a44816d89ca6c083
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-27 17:29:16 +0100

    FEAT stampa mail

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            mail: [],
+
+            numeroMail: '',
+        }
+    },
+
+    methods:{
+        getMail(){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res)=>{
+                this.mail.push(res.data.response)
+                // if(this.mail.lenght === parseInt(numeroMail)){
+                    
+                // }
+                // console.log(res.data.response)
+            })
+            console.log(this.mail)
+        },
+
+        displayArray(){
+            this.numeroMail=''
+            
+            for (let i=0; i < parseInt(this.numeroMail); i++){
+                this.getMail()
+            }
+
+        }
+    }
+
+}).mount('#app')
